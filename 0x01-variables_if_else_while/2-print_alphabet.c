eb040e6bbb6602aa0637aae330cb68765766a62d
berehan-fh <berehanfh@gmail.com>
2022-04-11 01:23:31 -0700

    2..alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,11 +10,14 @@
 
 int main(void)
 {
-	char low;
+	char c = 'a';
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar('\n');
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
 
+	putchar('\n');
 	return (0);
 }
