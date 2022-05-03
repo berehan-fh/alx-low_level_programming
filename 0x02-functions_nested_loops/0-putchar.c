644deb414da0df5677fdd7a71c9b2e1a9c07349d
berehan-fh <berehanfh@gmail.com>
2022-05-03 04:34:16 +0000

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,19 @@
 #include "main.h"
+
 /*
  * a function that prints -putchar
  *
  */
 int main(void)
 {
-	return(0);
+	char s[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+	return (0);
 }
