b68a1f4568bde3436ec07af415d1eab26c1026e2
berehan-fh <berehanfh@gmail.com>
2022-05-03 08:09:19 +0000

    Task-9 edited

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,22 +5,33 @@
  */
 void times_table(void)
 {
-	int i, j, mul;
+	int i, j, mul, digit_1, digit_2;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			mul = i * j;
-			if (mul < 9)
+			if (mul <= 9)
 			{
 				_putchar(mul + '0');
 			}
 			else
-
-			if (j < 9)
 			{
+				digit_2 = mul % 10;
+				digit_1 = mul / 10;
+				_putchar(digit_1 + '0');
+				_putchar(digit_2 + '0');
+			}
+			if (j < 9)
 				_putchar(',');
+			if (i * (j + 1) <= 9)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+			{
 				_putchar(' ');
 			}
 		}
