82780852978c354ad0367c447397ac94a969a708
berehan-fh <berehanfh@gmail.com>
2022-05-03 05:33:27 +0000

    Task 4

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _islower - checks for a lower case character
+ * @c: The character to be checked
+ *
+ * Return: 1 if c is lowercase 0 otherwise
+ */
+int _islower(int c);
+{
+	int k;
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		if (c == k)
+			return (1);
+		else
+			return (0);
+	}
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * _isalpha - checks for alphabetic characters
+ * @c: The character to be checked
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase... 0 Always
+ */
+int _isalpha(int c)
+{
+	int checker;
+
+	for (checker = 'a'; checker <= 'z'; checker++)
+	{
+		if (c == checker)
+			return (1);
+	}
+	for (checker = 'A'; checker <= 'Z'; checker++)
+	{
+		if (c == checker)
+			return (1);
+	}
+
+	return (0)
+}
+
