6a9c73015c603c942893ce8161158050ec8505f7
berehan-fh <berehanfh@gmail.com>
2022-05-06 04:19:57 +0000

    Task-1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+/**
+ * reset_to_98 - takes a pointer to an int and updates the value it points to98
+ * @n: pointer arguement
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x05-pointers _ arrays _ strings
